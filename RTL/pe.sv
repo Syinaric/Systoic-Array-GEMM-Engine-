8d8042b42cb0f7079e3ac46aab9ea646901a2cb7
Syinaric <Mahirarora21@gmail.com>
2026-08-18 15:13:34 -0400

    added ports

---
+++ RTL/pe.sv
@@ -0,0 +1,36 @@
+module pe_array #(
+    parameter int DATA_WIDTH = 8, 
+    parameter int ACC_WIDTH = 32 
+)(
+    input logic clk,
+    input logic reset,
+    input logic en
+    //west to east and north to south
+    input logic signed  [DATA_WIDTH -1 :0] a_in,
+    input logic signed  [DATA_WIDTH -1 :0] b_in,
+    output logic signed [DATA_WIDTH -1 :0]a_out,
+    output logic signed [DATA_WIDTH -1 :0]b_out,
+
+    input logic first_in, 
+    input logic last_in,
+    output logic first_out, 
+    output logic last_out,
+
+   //drain 
+    input logic drain_shift 
+    input  logic signed [ACC_WIDTH-1:0]   drain_in,
+    output logic signed [ACC_WIDTH-1:0]   drain_out
+
+); 
+
+
+
+
+
+
+
+
+
+
+
+)
